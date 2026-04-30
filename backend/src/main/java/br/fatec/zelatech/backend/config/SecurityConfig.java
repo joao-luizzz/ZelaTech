ba4f065a3997e75e69f81efc1528cdcc7a0fd745
joao-luizzz <luizjoaoprofissional@gmail.com>
2026-04-30 00:02:54 -0300

    feat: implement service layer (Usuario, Auth, Chamado, Aviso)

--- backend/src/main/java/br/fatec/zelatech/backend/config/SecurityConfig.java
+++ backend/src/main/java/br/fatec/zelatech/backend/config/SecurityConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,3 +31,4 @@ public class SecurityConfig {
         return http.build();
     }
 }
+
