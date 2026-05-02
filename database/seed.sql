7bb23d8bc5b0b5e7965fa275b97f93f7a7c798d5
João Luiz <luizjoaoprofissional@gmail.com>
2026-05-01 21:37:33 -0300

    Hash passwords and update user roles in seed data
    Updated user passwords to hashed format and changed user roles.

--- database/seed.sql
+++ database/seed.sql
@@ -3,9 +3,9 @@
 
 -- Inserindo Usuários (Senhas em texto plano para teste, mas no backend devem ser BCrypt)
 INSERT INTO usuario (nome, email, senha, apartamento, perfil) VALUES
-('João Síndico', 'sindico@zelatech.com', 'admin123', '101', 'SINDICO'),
-('Maria Moradora', 'maria@gmail.com', 'user123', '202', 'MORADOR'),
-('Carlos Morador', 'carlos@hotmail.com', 'user123', '303', 'MORADOR');
+('João Síndico', 'sindico@zelatech.com', '$2a$10$8.UnVuG9HHgffUDAlk8q6uy573U9EnYy4pSp9fPToW2Gf.pZfL7F6', '101', 'ROLE_SINDICO'),
+('Maria Moradora', 'maria@gmail.com', '$2a$10$8.UnVuG9HHgffUDAlk8q6uy573U9EnYy4pSp9fPToW2Gf.pZfL7F6', '202', 'ROLE_MORADOR'),
+('Carlos Morador', 'carlos@hotmail.com', '$2a$10$8.UnVuG9HHgffUDAlk8q6uy573U9EnYy4pSp9fPToW2Gf.pZfL7F6', '303', 'ROLE_MORADOR');
 
 -- Inserindo Avisos
 INSERT INTO aviso (titulo, conteudo, sindico_id) VALUES
@@ -14,9 +14,8 @@ INSERT INTO aviso (titulo, conteudo, sindico_id) VALUES
 
 -- Inserindo Chamados
 INSERT INTO chamado (titulo, descricao, categoria, prioridade, status, usuario_id) VALUES
-('Lâmpada Queimada', 'A lâmpada do corredor do 2º andar está queimada.', 'MANUTENCAO', 'BAIXA', 'ABERTO', 2),
-('Vazamento Garagem', 'Há um vazamento vindo do teto da vaga 45.', 'MANUTENCAO', 'ALTA', 'EM_ANDAMENTO', 3);
-
+('Lâmpada Queimada', 'A lâmpada do corredor do 2º andar está queimada.', 'ELETRICA', 'BAIXA', 'ABERTO', 2),
+('Vazamento Garagem', 'Há um vazamento vindo do teto da vaga 45.', 'HIDRAULICA', 'ALTA', 'EM_ANDAMENTO', 3);
 -- Inserindo Histórico de Status
 INSERT INTO historico_status (chamado_id, status_anterior, status_novo, usuario_id) VALUES
 (2, 'ABERTO', 'EM_ANDAMENTO', 1);
